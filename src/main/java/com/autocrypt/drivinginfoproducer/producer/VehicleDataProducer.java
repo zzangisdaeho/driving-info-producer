e43145bcde3ac7615c377f01bdac22ac2167a22d
DaeHo Kim <zzangisdaeho@naver.com>
2024-07-20 19:14:08 +0900

    condition version

--- src/main/java/com/autocrypt/drivinginfoproducer/producer/VehicleDataProducer.java
+++ src/main/java/com/autocrypt/drivinginfoproducer/producer/VehicleDataProducer.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -19,6 +20,7 @@ public class VehicleDataProducer {
 
     private static final String TOPIC = "gps-topic";
     private static final int NUM_VEHICLES = 1; // 생성할 차량 수
+    private static final int CONDITION_CHANGE_INTERVAL = 60 * 1000; // 60초
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
@@ -37,6 +39,8 @@ public class VehicleDataProducer {
     public void produceVehicleData(String vehicleId) {
         double currentLatitude = 37.7749;
         double currentLongitude = -122.4194;
+        long lastConditionChangeTime = System.currentTimeMillis();
+        String condition = "EMPTY";
 
         while (true) {
             try {
@@ -47,11 +51,18 @@ public class VehicleDataProducer {
                 currentLatitude += latitudeChange;
                 currentLongitude += longitudeChange;
 
+                long currentTime = System.currentTimeMillis();
+                if (currentTime - lastConditionChangeTime >= CONDITION_CHANGE_INTERVAL) {
+                    condition = "GUEST";
+                    lastConditionChangeTime = currentTime;
+                }
+
                 Map<String, Object> message = new HashMap<>();
                 message.put("vehicleId", vehicleId);
-                message.put("timestamp", Instant.now().toString());
+                message.put("timestamp", Instant.now().atZone(ZoneId.of("Asia/Seoul")).toString());
                 message.put("latitude", currentLatitude);
                 message.put("longitude", currentLongitude);
+                message.put("condition", condition);
 
                 String jsonMessage = objectMapper.writeValueAsString(message);
                 kafkaTemplate.send(TOPIC, vehicleId, message);
@@ -60,6 +71,9 @@ public class VehicleDataProducer {
 
                 // 5초 대기
                 Thread.sleep(5000);
+
+                // condition을 다시 "EMPTY"로 설정
+                condition = "EMPTY";
             } catch (Exception e) {
                 e.printStackTrace();
             }
