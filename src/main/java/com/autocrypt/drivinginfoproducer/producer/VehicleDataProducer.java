a8bdc8895c0c6c78361fe9e1fb62ad67868fe9ff
DaeHo Kim <zzangisdaeho@naver.com>
2024-07-20 23:27:36 +0900

    sample

--- src/main/java/com/autocrypt/drivinginfoproducer/producer/VehicleDataProducer.java
+++ src/main/java/com/autocrypt/drivinginfoproducer/producer/VehicleDataProducer.java
@@ -11,26 +11,30 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class VehicleDataProducer {
 
     private static final String TOPIC = "gps-topic";
-    private static final int NUM_VEHICLES = 1; // 생성할 차량 수
+    private static final int NUM_VEHICLES = 5; // 생성할 차량 수
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     private final ExecutorService executorService = Executors.newFixedThreadPool(NUM_VEHICLES);
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(NUM_VEHICLES);
 
     @PostConstruct
     public void startProducers() {
         for (int i = 0; i < NUM_VEHICLES; i++) {
             String vehicleId = "vehicle" + (i + 1);
             executorService.submit(() -> produceVehicleData(vehicleId));
+            scheduleStopMessage(vehicleId);
         }
     }
 
@@ -64,5 +68,24 @@ public class VehicleDataProducer {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void scheduleStopMessage(String vehicleId) {
+        long initialDelay = ThreadLocalRandom.current().nextLong(40, 61); // 0~40초 사이의 초기 딜레이
+        scheduledExecutorService.schedule(() -> produceStopMessage(vehicleId), initialDelay, TimeUnit.SECONDS);
+    }
+
+    private void produceStopMessage(String vehicleId) {
+        try {
+            kafkaTemplate.send(TOPIC, vehicleId, "STOP");
+
+            System.out.println("Produced STOP message: " + vehicleId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 랜덤한 시간(1~3분) 후에 다시 STOP 메시지를 스케줄링
+        long delay = ThreadLocalRandom.current().nextLong(40, 61); // 0~40초 사이의 랜덤 딜레이
+        scheduledExecutorService.schedule(() -> produceStopMessage(vehicleId), delay, TimeUnit.SECONDS);
     }
 }
